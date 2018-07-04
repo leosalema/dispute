6460366ba0fe3de62d4d80ea269d6e6a7f9727dd
leosalema <leosalema@yahoo.com.br>
2018-07-04 16:49:08 -0300

    remove routes

--- src/loader.js
+++ src/loader.js
@@ -1,2 +1,13 @@
-const server = require('./config/server')
-require ('./config/routes')(server)
+// const server = require('./config/server')
+// require ('./config/routes')(server)
+
+const express = require('express')
+const server = express()
+
+server.get('/', function(req, res) {
+    res.send('Hello World!')
+})
+const port = 5000
+server.listen(port, function() {
+    console.log(`BACKEND running on port ${port}`)
+})
